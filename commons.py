7f36ed97eac9ec5b1ede08f036ec164278118326
Mandeep <msg8930@yahoo.com>
2020-05-17 19:53:33 +0300

    find all working days in whole month

--- commons.py
+++ commons.py
@@ -9,3 +9,10 @@ def open_file(filename):
         return pd.read_csv(filename)
     except:
         print("File is Missing")
+
+
+def trading_days(df):
+    days_row = df['Date'].tolist()
+    days = []
+    [days.append(x) for x in days_row if x not in days]
+    return days
--- core.py
+++ core.py
@@ -1,8 +1,11 @@
-from commons import open_file
+from commons import open_file, trading_days
 
 symbol = 'DLF'
-expiry_date = '30-03-2019'
+expiry_date = '30-05-2019'
 
 optionchain_df = open_file("Data/{}-FUTSTK-{}.csv".format(symbol,expiry_date))
 
-print(optionchain_df)
+working_days = trading_days(optionchain_df)
+
+# print(optionchain_df)
+print(working_days)
